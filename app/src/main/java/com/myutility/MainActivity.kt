5980b885bc97abb24a9ab6720e5ec57d335ab6a2
Dwi Aji Cahyono <dwiaji724@gmail.com>
2023-01-27 22:37:21 +0700

    finalisasi aplikasi

--- app/src/main/java/com/myutility/MainActivity.kt
+++ app/src/main/java/com/myutility/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.myutility
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//handle button implicit
+        val btnBot : Button = findViewById(R.id.btn_bot)
+        btnBot.setOnClickListener{
+            val web = Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://dwibotapps.vercel.app/"))
+            startActivity(web)
+        }
+//        button storage
+        val btnStorage : Button = findViewById(R.id.btn_Storage)
+        btnStorage.setOnClickListener{
+            val web = Intent(Intent.ACTION_VIEW,
+                Uri.parse("isi link anda"))
+            startActivity(web)
+        }
     }
+
 }
